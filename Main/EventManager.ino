1ae8aee722402a8c209a459bec3832aeaa606999
Fritz Keyzer <fritzkeyzer@gmail.com>
2020-01-20 14:28:45 +0200

    Update: Wording of log messages upon startup noting the settings from ini file has been updated to start with 'setting' Log message for 'LITRES_ZONE_0X' updated to 'LITRES_ZONE_X' Log message for Seedling zone changed wording from 'ZONE 8' to 'ZONE seedling' Log messages added for manual control zone changes. Log messages added for seedling zone.

--- Main/EventManager.ino
+++ Main/EventManager.ino
@@ -10,6 +10,8 @@ bool flag_manualPower = false;
 int flag_manualZone = 0;
 bool flag_errorSD = false;
 
+bool flag_seedlingWatering = false;
+
 void event_update()
 {
 	//detect differences between flags and states
@@ -18,21 +20,21 @@ void event_update()
 							event_log("POWER", "ON", true);
 		char arr[16];
 		sprintf(arr, "%02d:%02d", setting_scheduleStartHour, setting_scheduleStartMinute);
-							event_log("SCHEDULE_START", arr, false);
+							event_log("SETTING_SCHEDULE_START", arr, false);
 		sprintf(arr, "%02d:%02d", setting_scheduleMinutesPerZoneLight/60, setting_scheduleMinutesPerZoneLight%60);
-							event_log("SCHEDULE_TIME_PER_ZONE_LIGHT", arr, false);
+							event_log("SETTING_SCHEDULE_TIME_PER_ZONE_LIGHT", arr, false);
 		sprintf(arr, "%02d:%02d", setting_scheduleMinutesPerZoneHeavy/60, setting_scheduleMinutesPerZoneHeavy%60);
-							event_log("SCHEDULE_TIME_PER_ZONE_HEAVY", arr, false);
+							event_log("SETTING_SCHEDULE_TIME_PER_ZONE_HEAVY", arr, false);
 		sprintf(arr, "%.2f", setting_flowVolumePerTick);
-							event_log("FLOW_VOLUME_PER_TICK", arr, false);
+							event_log("SETTING_FLOW_VOLUME_PER_TICK", arr, false);
 		sprintf(arr, "%02d:%02d", setting_seedlingInterval/60, setting_seedlingInterval%60);
-							event_log("SCHEDULE_SEEDLING_INTERVAL", arr, false);
+							event_log("SETTING_SCHEDULE_SEEDLING_INTERVAL", arr, false);
 		sprintf(arr, "%02d:%02d", setting_seedlingTime/60, setting_seedlingTime%60);
-							event_log("SCHEDULE_SEEDLING_TIME", arr, false);
+							event_log("SETTING_SCHEDULE_SEEDLING_TIME", arr, false);
 	}
 	
-						
-						
+	
+	
 	if (state_sequencePos != flag_sequencePos)
 	{
 		if (flag_sequencePos != -1)
@@ -41,7 +43,7 @@ void event_update()
 			char arr[16];
 			sprintf(arr, "%.0f", min(volume, 999999));
 			char arr2[16];
-			sprintf(arr2, "LITRES_ZONE_%02d", flag_sequencePos+1);
+			sprintf(arr2, "LITRES_ZONE_%1d", flag_sequencePos+1);
 							event_log(arr2, arr, false);
 			flow_resetVolume();
 		}
@@ -55,24 +57,86 @@ void event_update()
 			case (4):		event_log("ZONE", "5", 				true); 	break;
 			case (5):		event_log("ZONE", "6", 				true); 	break;
 			case (6):		event_log("ZONE", "7", 				true); 	break;
-			case (7):		event_log("ZONE", "8", 				true); 	break;
+			case (7):		event_log("ZONE", "SEEDLING", 		true); 	break;
+		}
+	}
+	
+	if (state_manualPower)
+	{
+		if (state_manualZone != flag_manualZone)
+		{
+			float volume = flow_getVolume();
+			char arr[16];
+			sprintf(arr, "%.0f", min(volume, 999999));
+			char arr2[16];
+			sprintf(arr2, "LITRES_ZONE_%1d", flag_manualZone+1);
+			if (flag_manualZone == 7)
+							event_log("LITRES_ZONE_SEEDLING", arr, 	false);
+			else
+							event_log(arr2, arr, 					false);
+			flow_resetVolume();
+			switch (state_manualZone)
+			{
+				case (-1):		event_log("IRRIGATION", "STOPPED", 	false); 	break;
+				case (0):		event_log("ZONE", "1", 				true); 	break;
+				case (1):		event_log("ZONE", "2", 				true); 	break;
+				case (2):		event_log("ZONE", "3", 				true); 	break;
+				case (3):		event_log("ZONE", "4", 				true); 	break;
+				case (4):		event_log("ZONE", "5", 				true); 	break;
+				case (5):		event_log("ZONE", "6", 				true); 	break;
+				case (6):		event_log("ZONE", "7", 				true); 	break;
+				case (7):		event_log("ZONE", "SEEDLING", 		true); 	break;
+			}
 		}
 	}
 	
 	if (state_manualPower && !flag_manualPower)
-							event_log("MODE", "MANUAL", true);
+	{
+							event_log("MODE", "MANUAL", 			true);
+		if (state_manualZone != 7)
+		{
+			char arr[1];
+			sprintf(arr, "%d", state_manualZone+1);
+							event_log("ZONE", arr, 					false);
+		}
+		else				event_log("ZONE", "SEEDLING", 			false);
+		
+	}
 	if (!state_manualPower && flag_manualPower)
-							event_log("MODE", "AUTOMATIC", true);
+	{
+		float volume = flow_getVolume();
+		char arr[16];
+		sprintf(arr, "%.0f", min(volume, 999999));
+		char arr2[16];
+		sprintf(arr2, "LITRES_ZONE_%1d", state_manualZone+1);
+		if (state_manualZone == 7)
+							event_log("LITRES_ZONE_SEEDLING", arr, 	false);
+		else
+							event_log(arr2, arr, 					false);
+							
+		flow_resetVolume();
+							event_log("MODE", "AUTOMATIC", 			true);
+	}
 	
-	
+	if (state_seedlingWatering && !flag_seedlingWatering)
+							event_log("SEEDLING", "START", 			true);
+	if (!state_seedlingWatering && flag_seedlingWatering)
+	{
+		float volume = flow_getVolume();
+		char arr[16];
+		sprintf(arr, "%.0f", min(volume, 999999));
+							event_log("LITRES_ZONE_SEEDLING", arr, 	false);
+		flow_resetVolume();
+							event_log("SEEDLING", "STOP", 			true);
+	}
 	
 	if (state_heavyDay && !flag_heavyDay)
 	{
-							event_log("DAY", "HEAVY_WATERING", true);
+							event_log("DAY", "HEAVY_WATERING", 		true);
 	}
 	else if (!state_heavyDay && flag_heavyDay)
 	{
-							event_log("DAY", "LIGHT_WATERING", true);
+							event_log("DAY", "LIGHT_WATERING", 		true);
 	}
 	if (!error_SD && flag_errorSD)
 	{
@@ -95,9 +159,9 @@ void event_update()
 	{
 		char arr[16];
 		sprintf(arr, "%d", state_soilMoisture);
-							event_log("SOIL_MOISTURE(%)", arr, false);
+							event_log("SOIL_MOISTURE(%)", arr,	 	false);
 		sprintf(arr, "%.2f", state_pressure);
-							event_log("WATER_PRESSURE(bar)", arr, false);
+							event_log("WATER_PRESSURE(bar)", arr, 	false);
 	}
 	
 	//update flags
@@ -107,7 +171,7 @@ void event_update()
 	flag_manualPower			= state_manualPower;
 	flag_manualZone				= state_manualZone;
 	flag_errorSD				= error_SD;
-	
+	flag_seedlingWatering		= state_seedlingWatering;
 }
 
 void event_log(String event, String value, bool display)
